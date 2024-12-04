456c24103c6c605bf3ba1399dd12f3335cfff98d
Rafeef87 <rafeefkhalifah1987@gmail.com>
2024-12-04 14:07:15 +0100

    Create a list of Contacts

---
+++ Business/Services/ContactService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Business.Models;
+
+namespace Business.Services;
+//Create a list of Contacts
+public class ContactService
+{
+    private List<Contact> _contacts = [];
+    //Create a Contact
+    public void CreateContact(Contact contact)
+    {
+        _contacts.Add(contact);
+    }
+    public IEnumerable<Contact> GetContacts()
+    {
+        return _contacts;
+    }
+}
