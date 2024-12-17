dfec2dc191da26597777064081adb77aeffa56e4
Rafeef87 <rafeefkhalifah1987@gmail.com>
2024-12-17 18:18:13 +0100

    Adding IFileService

---
+++ Business/Interfaces/IFileService.cs
@@ -0,0 +1,10 @@
+﻿
+using Business.Models;
+
+namespace Business.Interfaces;
+
+public interface IFileService
+{
+    void SaveListToFile(List<Contact> list);
+    List<Contact> GetAllLoadListFromFile();
+}
