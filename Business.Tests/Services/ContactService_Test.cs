735ddb6096adc8c9d57a14c3a53926ca24718d37
Rafeef87 <rafeefkhalifah1987@gmail.com>
2024-12-17 18:36:47 +0100

    Adding IContactService

---
+++ Business.Tests/Services/ContactService_Test.cs
@@ -0,0 +1,11 @@
+﻿
+using Business.Interfaces;
+using Moq;
+
+namespace Business.Tests.Services;
+
+public class ContactService_Test
+{
+    private readonly Mock<IFileService> _fileServiceMock;
+    private readonly 
+}
