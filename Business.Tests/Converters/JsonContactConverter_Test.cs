24d588df5aad299f92c3de344e25782c808ca156
Rafeef87 <rafeefkhalifah1987@gmail.com>
2024-12-19 13:25:05 +0100

    JsonContactConverter_Test

---
+++ Business.Tests/Converters/JsonContactConverter_Test.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Text.Json;
+using Business.Converters;
+using Business.Models;
+
+namespace Business.Tests.Converters;
+
+public class JsonContactConverter_Test
+{
+    [Fact]
+    public void ConvertToJson_ShouldConvertListToJson()
+    {
+        //Arrange
+        var contact = new Contact()
+        {
+            FirstName = "Rafeef",
+            LastName = "Khalifa",
+            Email = "rafeef@domin.com",
+            PhoneNumber = "1234567890",
+            StreetAddress = "123 Main st",
+            ZipCode = "12345",
+            Locality = "Cityville"
+        };
+        var list = new List<Contact>();
+
+        //Act
+        var result = JsonContactConverter.ConvertToJson(list);
+        //Assert
+        Assert.NotNull(result);
+    }
+    [Fact]
+    public void ConvertToList()
+    {
+        //Arrange
+        var contact = new Contact()
+        {
+            FirstName = "Rafeef",
+            LastName = "Khalifa",
+            Email = "rafeef@domin.com",
+            PhoneNumber = "1234567890",
+            StreetAddress = "123 Main st",
+            ZipCode = "12345",
+            Locality = "Cityville"
+        };
+        var list = new List<Contact>();
+        var json = JsonSerializer.Serialize(list);
+
+        //Act
+        var result = JsonContactConverter.ConvertToList(json);
+        //Assert
+        Assert.NotNull(result);
+        Assert.IsType<List<Contact>>(result);
+    }
+}
