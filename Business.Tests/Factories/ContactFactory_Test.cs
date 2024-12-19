793d2ae4a12f594f713830233a7e79353b54ea8f
Rafeef87 <rafeefkhalifah1987@gmail.com>
2024-12-19 13:00:58 +0100

    ContactFactory Teast

---
+++ Business.Tests/Factories/ContactFactory_Test.cs
@@ -0,0 +1,35 @@
+﻿using Business.Factories;
+using Business.Models;
+
+namespace Business.Tests;
+
+public class ContactFactory_Test
+{ 
+    [Fact]
+    public void CreateContact_ShouldReturnNewContact()
+    {
+        //Arrange
+        var contact = new ContactRegistrationForm()
+        {
+            FirstName = "Rafeef",
+            LastName = "Khalifa",
+            Email = "rafeef@domin.com",
+            PhoneNumber = "1234567890",
+            StreetAddress = "123 Main st",
+            ZipCode = "12345",
+            Locality = "Cityville"
+        };
+         //Act
+         var result = ContactFactory.Create(contact);
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(contact.FirstName, result.FirstName);
+        Assert.Equal(contact.LastName, result.LastName);
+        Assert.Equal(contact.Email, result.Email);
+        Assert.Equal(contact.PhoneNumber, result.PhoneNumber);
+        Assert.Equal(contact.StreetAddress, result.StreetAddress);
+        Assert.Equal(contact.ZipCode, result.ZipCode);
+        Assert.Equal(contact.Locality, result.Locality);
+
+    }
+}
